1df6597e5fbed8b0260bf34bd168d6b39efe37f0
jyj <busy.joj@gmail.com>
2023-08-10 21:29:19 +0900

    test2

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-s3`,
+      options: {
+        aws: {
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          },
+          region: process.env.AWS_REGION,
+        },
+        buckets: ["olivblog"],
+        expiration: 120,
+      },
+    },
   ],
 }
