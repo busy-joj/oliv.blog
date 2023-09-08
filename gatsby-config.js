d9fd1c650342363af3070965fc5ef97d0b3464aa
jyj <busy.joj@gmail.com>
2023-09-08 14:08:13 +0900

    posting

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,25 +3,28 @@
  */
 
 module.exports = {
-  siteMetadata: {
-    title: `oliv.blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `Jeong Younjung`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-mdx`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/`,
-        name: `src`,
-      },
-    }
-  ],
+    siteMetadata: {
+        title: `oliv.blog`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `Jeong Younjung`,
+        siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    },
+    plugins: [
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {},
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/`,
+                name: `src`,
+            },
+        },
+    ],
 }
--- src/components/aside.js
+++ src/components/aside.js
@@ -1,52 +1,85 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { FaGithub } from 'react-icons/fa6'
+import { FaGithub } from "react-icons/fa6"
 
-import './layout.css'
+import "./layout.css"
 
-const Aside = () => (
-    <div class="aside">
-        <div class="aside-img">
-            <span class="img">
-                <StaticImage src="../images/profile.jpg" loading="eager" alt=""/>
-            </span>
+const Aside = data => {
+    let { allMarkdownRemark } = data
+    let { group } = allMarkdownRemark
+    return (
+        <div class="aside">
+            <div class="aside-img">
+                <span class="img">
+                    <StaticImage
+                        src="../images/profile.jpg"
+                        loading="eager"
+                        alt=""
+                    />
+                </span>
+            </div>
+            <div class="aside-txt">
+                {/* <div className="main-intro">
+                <h2>Welcome!💫 {data.site.siteMetadata.title}</h2>
+                <p>안녕하세요!</p>
+                <p>지속가능한 프론트엔드 개발자 oliv입니다!</p>
+            </div> */}
+                {/* <h4>{data.allMarkdownRemark.totalCount} Post</h4> */}
+                <span class="name">category</span>
+                <ul>
+                    {group.map(v => (
+                        <li>{v}</li>
+                    ))}
+                    <li class="info">
+                        <Link to="/" className="info-link">
+                            All<span> (0)</span>
+                        </Link>
+                    </li>
+                    <li class="info">
+                        <Link to="/" className="info-link">
+                            Javascript
+                        </Link>
+                    </li>
+                    <li class="info">
+                        <Link to="/" className="info-link">
+                            Javascript
+                        </Link>
+                    </li>
+                    <li class="info">
+                        <Link to="/" className="info-link">
+                            Javascript
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div class="aside-bt">
+                <Link to="https://github.com/busy-joj">
+                    <FaGithub className="btn-icon" />
+                    <span className="blind">GitHub</span>
+                </Link>
+                <Link to="https://github.com/busy-joj">
+                    <FaGithub className="btn-icon" />
+                    <span className="blind">GitHub</span>
+                </Link>
+                <Link to="https://github.com/busy-joj">
+                    <FaGithub className="btn-icon" />
+                    <span className="blind">GitHub</span>
+                </Link>
+            </div>
         </div>
-        <div class="aside-txt">
-            {/* <div className="main-intro">
-					<h2>Welcome!💫 {data.site.siteMetadata.title}</h2>
-					<p>안녕하세요!</p>
-					<p>지속가능한 프론트엔드 개발자 oliv입니다!</p>
-				</div> */}
-            {/* <h4>{data.allMarkdownRemark.totalCount} Post</h4> */}
-            <span class="name">category</span>
-            <ul>
-                <li class="info">
-                    <Link to='/' className="info-link">All<span> (0)</span></Link>
-                </li>
-                <li class="info">
-                    <Link to='/' className="info-link">Javascript</Link>
-                </li>
-                <li class="info">
-                    <Link to='/' className="info-link">Javascript</Link>
-                </li>
-                <li class="info">
-                    <Link to='/' className="info-link">Javascript</Link>
-                </li>
-            </ul>
-        </div>
-        <div class="aside-bt">
-            <Link to="https://github.com/busy-joj">
-                <FaGithub className="btn-icon"/><span className="blind">GitHub</span>
-            </Link>
-            <Link to="https://github.com/busy-joj">
-                <FaGithub className="btn-icon"/><span className="blind">GitHub</span>
-            </Link>
-            <Link to="https://github.com/busy-joj">
-                <FaGithub className="btn-icon"/><span className="blind">GitHub</span>
-            </Link>
-        </div>
-    </div>
-)
+    )
+}
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(limit: 10) {
+            group(field: { frontmatter: { category: SELECT } }) {
+                fieldValue
+                totalCount
+            }
+        }
+    }
+`
 
 export default Aside
